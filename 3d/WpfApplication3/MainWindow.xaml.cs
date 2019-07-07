1ead74fce7bfa59fe503f25346e5590175fa8a3f
Vyacheslav <31929848+pRockrammer@users.noreply.github.com>
2019-07-07 12:00:35 +0300

    init

---
+++ 3d/WpfApplication3/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Windows.Media.Media3D;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+
+namespace WpfApplication3
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            //List<Point3D> list = new List<Point3D>();
+            //Point3D a = new Point3D();
+            //InitializeComponent();
+            //geom.Positions.Clear();
+            //geom.Positions.Add(a);
+        }
+    }
+}
